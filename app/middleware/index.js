db7c9a986f8ca038b1d2f433333be15a99002003
arronf2e <191446367@qq.com>
2018-01-04 14:32:55 +0800

    添加 Log

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -3,7 +3,12 @@ const path = require('path');
 const bodyParser = require('koa-bodyparser');
 const nunjucks = require('koa-nunjucks-2');
 const staticFiles = require('koa-static');
+// 引入日志中间件
+const miLog = require('./mi-log');
 module.exports = (app) => {
+
+  app.use(miLog());
+
   // 指定 public目录为静态资源目录，用来存放 js css images 等
   app.use(staticFiles(path.resolve(__dirname, "../public")));
 
